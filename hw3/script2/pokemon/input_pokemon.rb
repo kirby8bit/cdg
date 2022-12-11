9620f317382e219b710b89f20e268ff707f6157f
kirby8bit <kirby8bit@mail.ru>
2022-12-11 20:14:09 +0300

    fix tabs

--- hw3/script2/pokemon/input_pokemon.rb
+++ hw3/script2/pokemon/input_pokemon.rb
@@ -1,11 +1,11 @@
 module InputPokemon 
-	def input_name(number_of_pokemon)
+  def input_name(number_of_pokemon)
     puts "enter #{number_of_pokemon + 1} pokemon name: "
     gets.chomp
-	end 
+  end 
 
   def input_color(number_of_pokemon)
     puts "enter  #{number_of_pokemon + 1} pokemon color: "
     gets.chomp
-	end 
+  end 
 end
